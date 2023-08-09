6fd835a7fb5b1d70c8f5fd381cb5a1762e55f102
hardiki102 <141419197+hardiki102@users.noreply.github.com>
2023-08-09 17:15:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-       menu.classList.toggle('fa-times');
-       navbar.classList.toggle('active');
-}
