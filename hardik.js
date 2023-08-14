300ce2ab703681b272c621b9b1fefa8707bbe043
hardiki102 <141419197+hardiki102@users.noreply.github.com>
2023-08-14 10:42:29 +0530

    Update hardik.js

--- hardik.js
+++ hardik.js
@@ -2,6 +2,6 @@ let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelectorAll('.navbar');
 
 menu.onclick = () =>{
-       menu.classList.toggle('fa-times');
-       navbar.classList.toggle('active');
+       menu.classList.toggle('.fa-times');
+       navbar.classList.toggle('.active');
 }
