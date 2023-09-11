cf903b6af809f55e076181b86c05d21819eecc10
hardiki102 <141419197+hardiki102@users.noreply.github.com>
2023-09-11 09:16:55 +0530

    Update hardik.js

--- hardik.js
+++ hardik.js
@@ -7,3 +7,12 @@ $(document).ready(function(){
        });
        
 });
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
